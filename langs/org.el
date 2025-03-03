7002f5a4aa7c210320a5f30279445a0ae26a41fa
Manuel Herrmann <0@0x17.de>
2025-03-04 00:48:23 +0100

    disable ox-search-mode

--- langs/org.el
+++ langs/org.el
@@ -3,6 +3,6 @@
   (add-hook 'org-mode-hook
             (lambda ()
               (define-key org-mode-map (kbd "C-c .") 'org-time-stamp))))
-(use-package ox-search
-  :ensure nil
-  :hook (org-mode . ox-search-mode))
+;;(use-package ox-search
+;;  :ensure nil
+;;  :hook (org-mode . ox-search-mode))
