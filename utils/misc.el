3cacb35f670cb1dfc236076902942cc683192225
Manuel Herrmann <0@0x17.de>
2025-03-20 23:52:12 +0100

    add global git-gutter mode

--- utils/misc.el
+++ utils/misc.el
@@ -115,3 +115,7 @@
   (global-set-key (kbd "<C-M-S-down>")   'buf-move-down)
   (global-set-key (kbd "<C-M-S-left>")   'buf-move-left)
   (global-set-key (kbd "<C-M-S-right>")  'buf-move-right))
+
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode t))
