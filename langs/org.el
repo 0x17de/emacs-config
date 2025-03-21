656203424c63eca19bdcad4cd425987ef35b3626
Manuel Herrmann <0@0x17.de>
2025-03-21 21:25:28 +0100

    org: add functions to execute nushell & jinja2 templates

--- langs/org.el
+++ langs/org.el
@@ -17,3 +17,18 @@
 
 (use-package org-modern
   :hook ((org-mode . org-modern-mode)))
+
+(defun org-babel-execute:nushell (body params)
+  "Execute a block of nushell code"
+  (let* ((cmd (or (cdr (assoc :cmd params)) "nu"))
+         (args (or (cdr (assoc :args params)) "-c"))
+         (code (shell-quote-argument (org-babel-chomp body)))
+         (command (format "%s %s %s" cmd args code)))
+    (org-babel-eval command "")))
+
+(defun org-babel-execute:jinja (body params)
+  "Execute a jinja2 template"
+  (let* ((script (shell-quote-argument "import sys, jinja2 as j2; print(j2.Template(sys.argv[1]).render())"))
+         (code (shell-quote-argument (org-babel-chomp body)))
+         (command (format "python3 -c %s %s" script code)))
+    (org-babel-eval command "")))
