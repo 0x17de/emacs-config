a72ee577f7221c8bcf1104c18b02f5180b43efcd
Manuel Herrmann <0@0x17.de>
2025-03-26 23:24:43 +0100

    create unique vterm names on spawn

--- _0x17de-emacs.el
+++ _0x17de-emacs.el
@@ -23,9 +23,17 @@
 (load "utils/replace-with-shell")
 (load "utils/misc")
 
+(defvar _0x17de/vterm-counter 0
+  "Counter to make unique vterm buffers.")
+(defun _0x17de/start-vterm ()
+    "Starts a new vterm buffer."
+  (interactive)
+  (setq _0x17de/vterm-counter (1+ _0x17de/vterm-counter))
+  (let ((buffer-name (format "*vterm-%d*" _0x17de/vterm-counter)))
+    (vterm buffer-name)))
 (use-package vterm
   :ensure t
-  :bind (("C-M-S-x" . 'vterm)
+  :bind (("C-M-S-x" . _0x17de/start-vterm)
          :map vterm-mode-map
          ("C-c C-x" . 'vterm--self-insert)))
 
