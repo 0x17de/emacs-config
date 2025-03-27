db0c9335d4d6ae7b9b9d5e30edd767fde1842ce6
Manuel Herrmann <0@0x17.de>
2025-03-27 20:41:13 +0100

    org-mode: highlight tasks whose deadline falls on today

--- langs/org.el
+++ langs/org.el
@@ -4,6 +4,8 @@
   :bind
   (:map org-mode-map
         ("C-c ." . 'org-time-stamp))
+  :hook
+  (org-mode . _0x17de/org-setup-deadline-highlighting)
   :config
   (org-babel-do-load-languages
    'org-babel-load-languages
@@ -88,3 +90,43 @@ This shell is used to execute the Nushell command."
          (code (shell-quote-argument (org-babel-chomp body)))
          (command (format "python3 -c %s %s" script code)))
     (org-babel-eval command "")))
+
+(defface _0x17de/org-deadline-highlight
+  '((t . (:background "#4c1f24")))
+  "Highlight face for today's deadlines")
+
+(defun _0x17de/org-highlight-todays-deadlines ()
+  "Highlight headlines with a deadline of today in Org mode."
+  (interactive)
+  (save-excursion
+    (remove-overlays (point-min) (point-max) 'face '_0x17de/org-deadline-highlight)
+    (goto-char (point-min))
+    (let ((today (calendar-current-date))
+          (current-headline nil)
+          (current-headline-start nil)
+          (current-headline-end nil))
+      (while (re-search-forward "^\\*+ \\(.*\\)$\\|DEADLINE: <\\([^>]+\\)>" nil t)
+        (cond
+         ((match-string 1)
+          (setq current-headline (match-string-no-properties 1)
+                current-headline-start (match-beginning 0)
+                current-headline-end (line-end-position)))
+         ((match-string 2)
+          (ignore-errors
+            (let* ((current-deadline (match-string-no-properties 2))
+                   (deadline-parsed (org-parse-time-string current-deadline))
+                   (deadline-date-abs (calendar-absolute-from-gregorian
+                                       (list (nth 4 deadline-parsed)
+                                             (nth 3 deadline-parsed)
+                                             (nth 5 deadline-parsed))))
+                   (today-date-abs (calendar-absolute-from-gregorian today))
+                   (days-diff (- deadline-date-abs today-date-abs)))
+              (when (eq days-diff 0)
+                (let ((ov (make-overlay current-headline-start current-headline-end)))
+                  (overlay-put ov 'face '_0x17de/org-deadline-highlight)))))))))))
+
+(defun _0x17de/org-setup-deadline-highlighting ()
+  (when (derived-mode-p 'org-mode)
+    (add-hook 'after-save-hook #'_0x17de/org-highlight-todays-deadlines nil t)
+    (add-hook 'after-change-functions (lambda (&rest _) (_0x17de/org-highlight-todays-deadlines)) nil t)
+    (_0x17de/org-highlight-todays-deadlines))) ;; run once initially
