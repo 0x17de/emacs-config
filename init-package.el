8bcc6855cda4c5dd08d71edfbdba49d10840de81
Manuel Herrmann <0@0x17.de>
2025-04-02 00:09:26 +0200

    init-package: use-package is now built-in

--- init-package.el
+++ init-package.el
@@ -1,17 +1,7 @@
 (require 'package)
-;(setq-default load-prefer-newer t)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-;(setq-default package-enable-at-startup nil)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package t))
-
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (require 'use-package)
 (setq-default
  use-package-always-ensure t
  use-package-compute-statistics t
  use-package-verbose t)
-(use-package use-package-ensure-system-package
-  :defer t)
