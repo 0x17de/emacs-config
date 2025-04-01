d9312626e5140584fbfb45c5e23ca61f365690a2
Manuel Herrmann <0@0x17.de>
2025-04-02 00:34:38 +0200

    disable exec-path-from-shell per default via customize

--- _0x17de-emacs.el
+++ _0x17de-emacs.el
@@ -15,10 +15,18 @@
 (load "init-zoom-frm.el")
 (load "init-exwm.el")
 
-(use-package exec-path-from-shell
-  :demand t
-  :ensure t
-  :init (exec-path-from-shell-initialize))
+(defcustom _0x17de/exec-path-from-shell:enable nil
+  "Try to get the environment variables form shell."
+  :group '_0x17de
+  :type 'boolean)
+(when _0x17de/exec-path-from-shell:enable
+  (use-package exec-path-from-shell
+    :demand t
+    :ensure t
+    :init
+    (condition-case err
+        (exec-path-from-shell-initialize)
+      (error (message "Failed to get exec-path from shell: %S" err)))))
 
 (load "utils/indention")
 (load "utils/replace-with-shell")
