0e33e9f64948065388948a576473888a148bf11e
Manuel Herrmann <0@0x17.de>
2025-04-07 21:30:30 +0200

    update lisp mode; allow paredit-mode via customize

--- langs/lisp.el
+++ langs/lisp.el
@@ -1,24 +1,68 @@
+(defcustom _0x17de/lisp-paredit:enabled nil
+  "When enabled, this option automatically activates paredit mode in Lisp modes."
+  :group '_0x17de
+  :type 'boolean)
+
+(use-package slime
+  :ensure t
+  :defer t
+  :init
+  (setq inferior-lisp-program "sbcl")
+  :config
+  (slime-setup '(slime-fancy slime-company slime-quicklisp slime-asdf))
+  :bind (:map lisp-mode-map
+              ([tab] . slime-complete-symbol)
+              ("C-c C-d h" . slime-documentation-lookup)
+              ("C-c C-z" . slime-switch-to-output-buffer))
+  :hook ((lisp-mode . (lambda ()
+                        ;;(setq lisp-indent-function nil)
+                        (slime-mode t)))
+         (slime-repl-mode . (lambda ()
+                              (company-mode t)))))
+
 (use-package lisp-mode
   :ensure nil
-  :defer t
+  :demand t
   :bind
   (:map lisp-mode-map
         ([tab] . company-indent-or-complete-common))
   :hook
   (lisp-mode . (lambda ()
-                 (setq company-backends '(company-capf
+                 (setq company-backends '(company-slime
+                                          company-capf
                                           company-files))
                  (company-mode t))))
 
 (use-package elisp-mode
   :ensure nil
-  :defer t
-  :bind
-  (:map emacs-lisp-mode-map
-        ([tab] . company-indent-or-complete-common))
+  :demand t
+  :bind (:map emacs-lisp-mode-map ("<tab>" . 'company-indent-or-complete-common))
   :hook
   (emacs-lisp-mode . (lambda ()
-                       (setq company-backends '(company-elisp
-                                                company-capf
-                                                company-files))
-                       (company-mode t))))
+                       (company-mode t)
+                       (eldoc-mode t)
+                       (prettify-symbols-mode t)
+                       ;;(setq lisp-indent-function nil)
+                       (setq company-backends '(company-capf
+                                                company-files
+                                                company-dabbrev-code)))))
+
+(use-package slime-company
+  :ensure t
+  :after (slime company)
+  :config
+  (setq slime-company-completion 'fuzzy)
+  (setq slime-company-major-modes '(lisp-mode slime-repl-mode)))
+
+(when _0x17de/lisp-paredit:enabled
+  (use-package paredit
+    :ensure t
+    :defer t
+    ;;:diminish paredit-mode
+    :hook ((emacs-lisp-mode . paredit-mode)
+           (lisp-mode . paredit-mode)
+           (lisp-interaction-mode . paredit-mode)
+           (slime-repl-mode . paredit-mode))
+    :config
+    (define-key paredit-mode-map (kbd "C-M-f") 'paredit-forward)
+    (define-key paredit-mode-map (kbd "C-M-b") 'paredit-backward)))
