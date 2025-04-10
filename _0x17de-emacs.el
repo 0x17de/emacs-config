21a8a872576da4b621daefc50e1f6d905e1ab342
Manuel Herrmann <0@0x17.de>
2025-04-10 23:51:35 +0200

    move modeline to separate config

--- _0x17de-emacs.el
+++ _0x17de-emacs.el
@@ -34,29 +34,11 @@
 (load "utils/misc")
 (load "utils/bolt")
 (load "utils/vterm")
+(load "utils/modeline")
 
 (use-package all-the-icons
   :ensure t
   :demand t)
-
-(defcustom _0x17de/modeline 'doom-modeline
-  "The modeline to use.
-Options are 'doom-modeline or 'smart-mode-line."
-  :type '(choice
-          (const :tag "Doom modeline" doom-modeline)
-          (const :tag "Smart mode line" smart-mode-line))
-  :group '_0x17de)
-(cond
- ((eq _0x17de/modeline 'doom-modeline)
-  (use-package doom-modeline
-    :ensure t
-    :init
-    (doom-modeline-mode t)))
- ((eq _0x17de/modeline 'smart-mode-line)
-  (use-package smart-mode-line
-    :ensure t
-    :config
-    (sml/setup))))
 
 (load "utils/multiple-cursors")
 (use-package refine)
---
+++ utils/modeline.el
@@ -0,0 +1,18 @@
+(defcustom _0x17de/modeline 'doom-modeline
+  "The modeline to use.
+Options are 'doom-modeline or 'smart-mode-line."
+  :type '(choice
+          (const :tag "Doom modeline" doom-modeline)
+          (const :tag "Smart mode line" smart-mode-line))
+  :group '_0x17de)
+(cond
+ ((eq _0x17de/modeline 'doom-modeline)
+  (use-package doom-modeline
+    :ensure t
+    :init
+    (doom-modeline-mode t)))
+ ((eq _0x17de/modeline 'smart-mode-line)
+  (use-package smart-mode-line
+    :ensure t
+    :config
+    (sml/setup))))
