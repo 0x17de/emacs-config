161fdfb848437e58298f3423f0932ae3280f35f5
Manuel Herrmann <0@0x17.de>
2025-04-11 21:38:15 +0200

    fix function to add multiple cursors on non-empty lines

--- utils/multiple-cursors.el
+++ utils/multiple-cursors.el
@@ -1,25 +1,24 @@
-(defun ox/add-multiple-cursors-to-non-empty-lines (start end)
+(defun _0x17de/add-multiple-cursors-to-non-empty-lines (start end)
   "Add a cursor in each non-empty line of selection"
   (interactive "r")
-  (require 'multiple-cursors)
-  (let ((original-point (point)))
-    (save-excursion
+  (save-excursion
+    (let ((start (min (mark) (point)))
+          (end (max (mark) (point))))
       (goto-char start)
+      (beginning-of-line)
       (while (< (point) end)
-        (let ((line-begin (line-beginning-position))
-              (line-end (line-end-position)))
-          (unless (or (eq line-begin line-end)
-                      (and (>= original-point line-begin)
-                           (<= original-point line-end)))
-            (mc/mark-next-like-this 1))
-          (goto-char (min (1+ line-end) end))))
-      (mc/maybe-multiple-cursors-mode))))
+        (unless (string-match-p "^\s*$" (buffer-substring-no-properties
+                                     (line-beginning-position)
+                                     (line-end-position)))
+          (mc/create-fake-cursor-at-point))
+        (forward-line 1))))
+  (mc/maybe-multiple-cursors-mode))
 (use-package multiple-cursors
   :init
   (global-unset-key (kbd "M-<down-mouse-1>"))
   :bind
   (("C-M-z C-c" . mc/edit-lines)
-   ("C-M-z C-M-c" . ox/add-multiple-cursors-to-non-empty-lines)
+   ("C-M-z C-M-c" . _0x17de/add-multiple-cursors-to-non-empty-lines)
    ("C-M-z >" . mc/mark-next-like-this)
    ("C-M-z <" . mc/mark-previous-like-this)
    ("C-M-z |" . mc/mark-all-like-this)
