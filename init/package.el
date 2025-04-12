197f70c48d307d6d2e9c979ce0922ff482c5af90
Manuel Herrmann <0@0x17.de>
2025-04-13 00:14:33 +0200

    add the packages directory into load-path

--- init/package.el
+++ init/package.el
@@ -6,3 +6,8 @@
  use-package-always-ensure t
  use-package-compute-statistics t
  use-package-verbose t)
+
+(let* ((base-dir (file-name-parent-directory (file-name-directory (or load-file-name buffer-file-name))))
+       (packages-dir (expand-file-name "packages" base-dir)))
+  (when (file-directory-p packages-dir)
+    (add-to-list 'load-path packages-dir)))
