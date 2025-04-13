3e1ebdb743f5deeb326f1fabdfc7b63ea8c554a6
Manuel Herrmann <0@0x17.de>
2025-04-13 23:22:42 +0200

    add company-ansible

--- langs/ansible.el
+++ langs/ansible.el
@@ -2,4 +2,10 @@
   :defer t
   :bind
   (:map ansible-key-map
-        ([f1] . 'ansible-doc)))
+        ([f1] . 'ansible-doc)
+        ([tab] . 'company-indent-or-complete-common))
+  :hook (ansible-mode . (lambda ()
+                          (add-to-list 'company-backends 'company-ansible)
+                          (company-mode t))))
+(use-package company-ansible
+  :defer t)
