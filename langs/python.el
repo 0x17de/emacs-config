75272f94e13297283b3257837c2a19333b502125
Manuel Herrmann <0@0x17.de>
2025-05-26 23:19:32 +0200

    python-mode: add hotkeys

--- langs/python.el
+++ langs/python.el
@@ -13,7 +13,37 @@ When non-nil, py-isort-buffer will be called before saving Python files."
               ([tab] . company-indent-or-complete-common)
               ([f1] . lsp-describe-thing-at-point)
               ([f12] . lsp-find-definition)
-              ("S-<f12>" . lsp-find-references))
+              ("S-<f12>" . lsp-find-references)
+              ;; Imports
+              ("C-c i s" . py-isort-buffer)
+              ;; Formatting
+              ("C-c f b" . python-black-buffer)
+              ("C-c f r" . python-black-region)
+              ("C-c f a" . py-autopep8-buffer)
+              ;; REPL
+              ("C-c C-p" . run-python)
+              ("C-c C-r" . python-shell-send-region)
+              ("C-c C-b" . python-shell-send-buffer)
+              ("C-c C-l" . python-shell-send-file)
+              ;; Documentation
+              ("C-c h h" . python-eldoc-at-point)
+              ("C-c h d" . python-describe-at-point)
+              ("C-c h f" . python-info-lookup-symbol)
+              ;; Navigation
+              ("M-." . xref-find-definitions)
+              ("M-," . xref-pop-marker-stack)
+              ("M-?" . xref-find-references)
+              ("C-c r" . lsp-find-references)
+              ("C-M-." . lsp-find-implementation)
+              ("C-M-," . lsp-find-type-definition)
+              ("C-c n" . python-nav-forward-defun)
+              ("C-c p" . python-nav-backward-defun)
+              ;; Errors
+              ("C-c e n" . flycheck-next-error)
+              ("C-c e p" . flycheck-previous-error)
+              ("C-c e l" . flycheck-list-errors)
+              ("C-c e c" . flycheck-clear)
+              ("C-c e v" . flycheck-verify-setup))
   :hook ((python-mode . (lambda ()
                           (setq yas-indent-line 'fixed)
                           (company-mode t)
